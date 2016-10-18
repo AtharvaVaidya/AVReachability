d10eaf0d6d03ee7fd505bfdff36d1fda1b271b75
TechieCSG <atharva.vaidya01@gmail.com>
2016-10-17 20:19:37 -0400

    Update to Swift 3

--- Pod/Classes/AVReachability.swift
+++ Pod/Classes/AVReachability.swift
@@ -9,20 +9,23 @@
 import Foundation
 import SystemConfiguration
 
-public class Reachability
+open class Reachability
 {
-    public class func isConnectedToNetwork() -> Bool
+    open class func isConnectedToNetwork() -> Bool
     {
         
         var zeroAddress = sockaddr_in()
-        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
         zeroAddress.sin_family = sa_family_t(AF_INET)
         
-        guard let defaultRouteReachability = withUnsafePointer(&zeroAddress, {
-            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
-        }) else {
-            return false
-        }
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress,
+        {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        })
+        
+        else { return false }
         
         var flags : SCNetworkReachabilityFlags = []
         if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags)
@@ -30,8 +33,8 @@ public class Reachability
             return false
         }
         
-        let isReachable = flags.contains(.Reachable)
-        let needsConnection = flags.contains(.ConnectionRequired)
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
         return (isReachable && !needsConnection)
     }
     
