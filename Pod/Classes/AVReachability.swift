74ebe69b79f6a683dce3f952a7260c23e0ef18e7
Atharva Vaidya <atharva.vaidya@icloud.com>
2019-06-29 18:01:16 -0500

    Updated for Swift 5.0

--- Pod/Classes/AVReachability.swift
+++ Pod/Classes/AVReachability.swift
@@ -9,33 +9,31 @@
 import Foundation
 import SystemConfiguration
 
-open class Reachability
-{
-    open class func isConnectedToNetwork() -> Bool
-    {
-        
+open class Reachability {
+    open class func isConnectedToNetwork() -> Bool {
         var zeroAddress = sockaddr_in()
-        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
         zeroAddress.sin_family = sa_family_t(AF_INET)
         
-        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress,
-        {
-            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
-                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
             }
-        })
+        }) else {
+            return false
+        }
         
-        else { return false }
-        
-        var flags : SCNetworkReachabilityFlags = []
-        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags)
-        {
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        if flags.isEmpty {
             return false
         }
         
         let isReachable = flags.contains(.reachable)
         let needsConnection = flags.contains(.connectionRequired)
+        
         return (isReachable && !needsConnection)
     }
-    
 }
